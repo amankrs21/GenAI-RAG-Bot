cb4d6cef06569fe94acaec4e4e0783fc8b0d428b
Aman Singh <amankrs212@gmail.com>
2025-05-15 01:54:09 +0530

    Refactor Router component to correct route paths and ensure proper navigation

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -7,19 +7,20 @@ import ServerUnavl from './pages/error/ServerUnavl';
 import PageNotFound from './pages/error/PageNotFound';
 import PrivateRoutes from './middleware/PrivateRoutes';
 import Feedback from './pages/feedback/Feedback';
+import ChatMainLayout from './pages/chat/ChatMainLayout';
 
 
 export default function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/404" element={<PageNotFound />} />
                 <Route path="/503" element={<ServerUnavl />} />
+                <Route path="/404" element={<PageNotFound />} />
+                <Route path="/chat" element={<ChatMainLayout />} />
                 <Route path="/welcome" element={<Welcome />} />
                 <Route path='*' element={<Navigate to='/404' />} />
-                <Route path="/" element={<Navigate to="/welcome" />} />
+                <Route path="/" element={<Navigate to="/chat" />} />
                 <Route path="/" element={<PrivateRoutes />}>
-                    <Route path="/chat" element={<ChatHome />} />
                     <Route path="/source" element={<FileMain />} />
                     <Route path="/feedback" element={<Feedback />} />
                 </Route>
