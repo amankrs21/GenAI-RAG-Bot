23287529dd3cbedf232ac8a116aac38fa80e851b
Aman Singh <amankrs212@gmail.com>
2025-05-19 02:16:15 +0530

    Refactor imports in App, Router, and index files for consistency and clarity

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-import ChatHome from './pages/chat/ChatHome';
 import Welcome from './pages/welcome/Welcome';
 import FileMain from './pages/files/FilesMain';
+import Feedback from './pages/feedback/Feedback';
 import ServerUnavl from './pages/error/ServerUnavl';
 import PageNotFound from './pages/error/PageNotFound';
 import PrivateRoutes from './middleware/PrivateRoutes';
-import Feedback from './pages/feedback/Feedback';
 import ChatMainLayout from './pages/chat/ChatMainLayout';
 
 
+// Router Component
 export default function Router() {
     return (
         <BrowserRouter>
@@ -17,12 +17,12 @@ export default function Router() {
                 <Route path="/503" element={<ServerUnavl />} />
                 <Route path="/404" element={<PageNotFound />} />
                 <Route path="/chat" element={<ChatMainLayout />} />
-                <Route path="/welcome" element={<Welcome />} />
+                <Route path="/admin" element={<Welcome />} />
                 <Route path='*' element={<Navigate to='/404' />} />
                 <Route path="/" element={<Navigate to="/chat" />} />
                 <Route path="/" element={<PrivateRoutes />}>
-                    <Route path="/source" element={<FileMain />} />
-                    <Route path="/feedback" element={<Feedback />} />
+                    <Route path="/admin/source" element={<FileMain />} />
+                    <Route path="/admin/feedback" element={<Feedback />} />
                 </Route>
             </Routes>
         </BrowserRouter>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from './App.jsx'
+import App from './App.jsx';
 
+
+// Main entry point for the React application
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
