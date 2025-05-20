1c1be620c7bfbfe48a7ca84c81eaff2e295f9622
Aman Singh <amankrs212@gmail.com>
2025-05-21 00:56:59 +0530

    Refactor PrivateRoutes and AuthContext for improved loading state handling and remove unused components

--- client/src/middleware/PrivateRoutes.jsx
+++ client/src/middleware/PrivateRoutes.jsx
@@ -1,73 +1,39 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
 
 import { useAuth } from '../hooks/useAuth';
-import AppHeader from '../layout/AppHeader';
-import NavbarActions from '../layout/NavbarActions';
+import Header from '../layout/Header';
 
 
 // PrivateRoutes component to protect routes
 export default function PrivateRoutes() {
 
     const navigate = useNavigate();
-    const { isAuthLoading, isAuthenticated, http } = useAuth();
-
-    const tempAuth = true;
-
-    const [chatId, setChatId] = useState(null);
-    const [history, setHistory] = useState([]);
-    const [messages, setMessages] = useState([]);
-
-    const handleOpenHistory = async () => {
-        console.log("Opening chat history...");
-    }
+    const { isAuthLoading, isAuthenticated } = useAuth();
 
     useEffect(() => {
-        if (!isAuthenticated) { navigate('/'); };
-        if (isAuthLoading || !http.defaults.headers.common.Authorization) return;
 
+        if (!isAuthenticated) {
+            navigate('/');
+        };
 
-        // const urlParams = new URLSearchParams(window.location.search);
-        // setChatId(urlParams.get('chatId') ?? null);
+        if (isAuthLoading) return;
 
-    }, [isAuthLoading, isAuthenticated, http, navigate]);
+    }, [isAuthLoading, isAuthenticated, navigate]);
 
     if (!isAuthenticated) {
         return null;
     }
 
-    // useEffect(() => {
-    //     const urlParams = new URLSearchParams(window.location.search);
-    //     setChatId(urlParams.get('chatId') ?? null);
-
-    // }, [chatId]);
-
-
     return (
         <>
-            {tempAuth ? (
-                // {!isAuthLoading && isAuthenticated && http.defaults.headers.common.Authorization ? (
-                <div className='main'>
-                    <div className='main__left'>
-                        <AppHeader />
-                    </div>
+            {!isAuthLoading && isAuthenticated ? (
 
+                <>
+                    <Header />
                     <Outlet />
-                    {/* <div className='main__center'>
-                    </div> */}
+                </>
 
-                    <div className='main__right'>
-                        <NavbarActions
-                            chatId={chatId}
-                            history={history}
-                            setChatId={setChatId}
-                            setHistory={setHistory}
-                            setMessages={setMessages}
-                            handleOpen={(e) => handleOpenHistory(e)}
-                        />
-                    </div>
-
-                </div>
             ) :
                 null
             }
