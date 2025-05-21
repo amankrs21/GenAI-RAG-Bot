2fa8b9eed469b49b800f7e96caeef5dd8a55a3cf
Aman Singh <amankrs212@gmail.com>
2025-05-22 00:11:23 +0530

    Add feedback deletion functionality and enhance FeedbackDialog component

--- client/src/pages/feedback/FeedbackDialog.jsx
+++ client/src/pages/feedback/FeedbackDialog.jsx
@@ -1,35 +1,46 @@
 import PropTypes from 'prop-types';
-import { Close } from '@mui/icons-material';
+import { Close, DeleteForever } from '@mui/icons-material';
 import { Dialog, DialogContent, Typography, IconButton, Button } from '@mui/material';
 
 
 // FeedbackDialog component
-export default function FeedbackDialog({ open, feedDialog, handleClose }) {
+export default function FeedbackDialog({ open, feedDialog, handleClose, handleDelete }) {
     return (
         <Dialog
-            open={open}
-            keepMounted
             fullWidth
-            maxWidth="sm"
+            keepMounted
+            open={open}
+            maxWidth="md"
             onClose={handleClose}
             sx={{ '& .MuiDialog-paper': { borderRadius: '8px', backgroundColor: '#ececec' } }}
         >
             <DialogContent
                 sx={{
+                    padding: 2,
                     display: 'flex',
                     flexDirection: 'column',
                     backgroundColor: 'inherit',
                 }}
             >
                 <Typography variant="h5" color='primary' textAlign='center' fontWeight={600} >
-                    USER FEEDBACK
+                    USER NEGETIVE FEEDBACK
                 </Typography>
                 <Typography variant='subtitle1'>
                     <b>User Message:</b> {feedDialog?.userMessage}<br />
-                    <b>Bot Message:</b> {feedDialog?.botMessage}<br />
-                    <b>User Comment:</b> {feedDialog?.comment}<br />
+                    <b>Bot Response:</b> {feedDialog?.botResponse}<br />
+                    <b>User Feedback:</b> {feedDialog?.comment}<br />
                     <b>Feedback Date:</b> {new Date(feedDialog?.createdAt).toLocaleString()}
                 </Typography>
+                <Button
+                    size='small'
+                    color='error'
+                    variant='contained'
+                    sx={{ marginTop: 2 }}
+                    onClick={handleDelete}
+                    endIcon={<DeleteForever />}
+                >
+                    DELETE THIS FEEDBACK
+                </Button>
             </DialogContent>
             <IconButton
                 size='small'
@@ -39,20 +50,13 @@ export default function FeedbackDialog({ open, feedDialog, handleClose }) {
             >
                 <Close />
             </IconButton>
-            <Button
-                margin={2}
-                variant='contained'
-                color='error'
-                size='small'
-                onClick={handleClose}>
-                DELETE THIS FEEDBACK
-            </Button>
         </Dialog>
     )
 }
 
 FeedbackDialog.propTypes = {
     open: PropTypes.bool.isRequired,
+    feedDialog: PropTypes.object.isRequired,
     handleClose: PropTypes.func.isRequired,
-    feedDialog: PropTypes.object.isRequired
+    handleDelete: PropTypes.func.isRequired,
 }
